fa2ff2f1906ebfdb6b74aa85172a634e93e9f810
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-06-28 12:48:11 +0300

    init: start test task

---
+++ config/webpack.dev.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const paths = require('./paths');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true,
+    },
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
