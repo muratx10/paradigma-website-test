9df7384411f4064d2dd7b0c360b4f6056928171e
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-06-29 22:12:00 +0300

    feat: finish header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 import '../scss/main.scss';
+import logo from '../assets/images/logo.png';
+
+document.querySelector('.logo img').src = logo;
+
+document.querySelector('.burger').addEventListener('click', (e) => {
+  const nav = document.querySelector('.navigation ul');
+  e.currentTarget.classList.toggle('burger-menu__active');
+  nav.classList.toggle('hidden');
+});
+
+function toggle() {
+  const nav = document.querySelector('.navigation ul');
+  document.querySelector('.burger').classList.toggle('burger-menu__active');
+  nav.classList.toggle('hidden');
+}
+
+document.querySelectorAll('.navigation__link').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggle(e);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const sass = require('sass');
+
+const conf = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    watchContentBase: true,
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        },
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+      sourceMap: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.ejs',
+      title: 'Paradigma Website',
+      favicon: './src/assets/favicon.png',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+
+};
+
+module.exports = (env, options) => {
+  conf.devtool = options.mode === 'production'
+    ? false
+    : 'eval-source-map';
+  return conf;
+};
