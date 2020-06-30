b038325ac9f6f50ac8edf162bdb5d71afc7bf923
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-06-30 22:08:29 +0300

    feat: add main section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
+import '../scss/header.scss';
 import '../scss/main.scss';
 import logo from '../assets/images/logo.png';
+import girl from '../assets/images/girl.png'
+import branchImg from '../assets/images/branch.png';
+import info from '../assets/images/card.png';
 
 document.querySelector('.logo img').src = logo;
+document.querySelector('.girl-image img').src = girl;
+document.querySelector('.get-card img').src = branchImg;
+document.querySelector('#info-card').src = info;
 
 document.querySelector('.burger').addEventListener('click', (e) => {
   const nav = document.querySelector('.navigation ul');
@@ -34,4 +41,25 @@ document.querySelector('.order__link').addEventListener('click', () => {
 document.querySelector('#submit').addEventListener('click', (e) => {
   document.querySelector('.order-modal').classList.remove('show-modal');
   document.querySelector('.order-modal').style.display = 'none';
+  document.querySelector('.submit-modal').classList.toggle('show-modal');
+});
+
+document.addEventListener('click', e => {
+  e.preventDefault();
+  const submitPopup = document.querySelector('.submit-modal');
+  if (e.target.closest('.show-modal')) {
+    if (e.target.closest('.show-modal').classList.contains('submit-modal')) {
+      submitPopup.classList.remove('show-modal');
+    }
+  }
+});
+
+document.querySelector('.icon-info').addEventListener('mouseover', () => {
+  document.querySelector('.info-text').classList.remove('hide-info');
+  document.querySelector('.info-text').classList.add('show-info');
+});
+
+document.querySelector('.icon-info').addEventListener('mouseout', () => {
+  document.querySelector('.info-text').classList.remove('show-info');
+  document.querySelector('.info-text').classList.add('hide-info');
 });
